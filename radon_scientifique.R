14d53cd552e657f5b313caa39470938b2159eec1
Arthur <135703612+Alglior@users.noreply.github.com>
2026-01-22 14:16:14 +0100

    Update radon_scientifique.R

--- radon_scientifique.R
+++ radon_scientifique.R
@@ -13,10 +13,13 @@ data <- read.csv("discours_scientifique_plus.csv", sep = ";", header = TRUE)
 
 #garder seuelement display_name ; abstract ; top_concepts
 
-data_work <- data %>%
-  select(display_name, abstract, top_concepts,publication_year)
+data_work <- subset(data, select = c(
+  display_name,
+  abstract,
+  top_concepts,
+  publication_year
+))
 
-# --- Création des périodes temporelles ---
 data_work <- data_work %>%
   mutate(periode = case_when(
     publication_year >= 1950 & publication_year <= 1990 ~ "1950-1990",
@@ -26,38 +29,44 @@ data_work <- data_work %>%
     TRUE ~ "Autre"
   ))
 
-
 #dans data_work des que l'on voit le mot radon ont garde la ligne sinon ont supprime
-
 data_radon_filter <- data_work %>%
   filter(grepl("radon", abstract, ignore.case = TRUE) | grepl("radon", top_concepts, ignore.case = TRUE))
 
 
-#tokenisation des mots dans les abstracts
-radon_abstract_token<- data_radon_filter %>% 
-  tidytext::unnest_tokens(output="mot_abstract",
-                          input=abstract,
-                          token="ngrams",
-                          n=1) 
+radon_abstract_token <- data_radon_filter %>% tidytext::unnest_tokens(output="mot_abstract",
+                                                                      input=abstract,
+                                                                      token="ngrams",
+                                                                      n=1) 
 
 radon_abstract_token_trie <- radon_abstract_token %>%
   anti_join(tidytext::stop_words,by=c("mot_abstract"="word"))
 
-radon_abstract_token_english<- mixr::get_lexicon("en")
+english_lexicon <- get_lexicon(language = "en")
+
+work_type <- merge(radon_abstract_token_trie, english_lexicon,by.x = "mot_abstract",
+                   by.y = "word")
+
+radon_abstract_token_trie <- subset(work_type, select = c(
+  display_name,
+  top_concepts,
+  publication_year,
+  periode,
+  lemma
+))
 
 freq_lemmes <- radon_abstract_token_trie %>%
-  group_by(mot_abstract, periode) %>% 
+  group_by(lemma,periode) %>% 
   summarise(freq=n()) %>% 
   arrange(desc(freq)) %>% 
   na.omit()
 head(freq_lemmes)
 
-#correlation en fonction des année de publication
-mots_corelation <- radon_abstract_token_trie %>% 
-  widyr::pairwise_count(mot_abstract,feature=publication_year,sort=TRUE)
 
+#correlation en fonction des annÃ©e de publication
+mots_corelation <- radon_abstract_token_trie %>% 
+  widyr::pairwise_count(lemma,feature=publication_year,sort=TRUE)
 
 radon_specificities <- mixr::tidy_specificities(radon_abstract_token_trie,
-                                     mot_abstract,
-                                     periode)
-head(radon_specificities)
+                                                lemma,
+                                                periode)
